f662657886c70d4dcc6f896eae4c5a6fa113c00a
kalbek <access2kaleb@gmail.com>
2023-07-21 09:35:27 +0300

    Correct the foreing keys addition to models.

--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_115044) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_21_063030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
   enable_extension "plpgsql"
 
-  create_table "comment", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "post_id"
+  create_table "comments", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_comment_on_author_id"
-    t.index ["post_id"], name: "index_comment_on_post_id"
   end
 
-  create_table "like", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "post_id"
+  create_table "likes", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_like_on_author_id"
-    t.index ["post_id"], name: "index_like_on_post_id"
   end
 
-  create_table "post", force: :cascade do |t|
-    t.bigint "author_id"
+  create_table "posts", force: :cascade do |t|
+    t.integer "author_id"
     t.string "title"
     t.text "text"
     t.integer "comments_counter"
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_post_on_author_id"
   end
 
-  create_table "user", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "photo"
-    t.text "bio"
-    t.integer "posts_counter"
+    t.string "bio"
+    t.string "posts_counter"
+    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comment", "post"
-  add_foreign_key "comment", "user", column: "author_id"
-  add_foreign_key "like", "post"
-  add_foreign_key "like", "user", column: "author_id"
-  add_foreign_key "post", "user", column: "author_id"
 end
