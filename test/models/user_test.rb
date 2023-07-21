65106eb12b01c33583bedbca2f6ab027395bd417
kalbek <access2kaleb@gmail.com>
2023-07-21 17:02:57 +0300

    Add test for User and Post class methods.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name must not be blank' do
+    user = User.new(name: '')
+    assert_not user.valid?
+  end
+
+  test 'posts_counter must be an integer greater than or equal to zero' do
+    user = User.new(posts_counter: -1)
+    assert_not user.valid?
+
+    user.posts_counter = 0
+    assert user.valid?
+
+    user.posts_counter = 1
+    assert user.valid?
+
+    user.posts_counter = 'not a number'
+    assert_not user.valid?
+  end
+
+  test 'recent_posts returns the most recent posts' do
+    user = create(:user)
+    create_list(:post, 5, author: user)
+    assert_equal 3, user.recent_posts.count
+  end
 end
