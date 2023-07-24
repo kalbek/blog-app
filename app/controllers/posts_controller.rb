657d0159cc48ea59d42da781434a9387a71560ae
kalbek <access2kaleb@gmail.com>
2023-07-24 12:04:52 +0300

    Add index action for post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    # Add any additional logic you want for displaying all posts
+  end
+  
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
