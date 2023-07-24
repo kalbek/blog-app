2be50e718db259c10f2ac24f69bda21e40d20eec
kalbek <access2kaleb@gmail.com>
2023-07-24 11:41:08 +0300

    Check the handled urls and fix linters.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,8 @@
 # app/controllers/posts_controller.rb
 class PostsController < ApplicationController
-    def show
-      @user = User.find(params[:user_id])
-      @post = @user.posts.find(params[:id])
-      # Add any logic you want to perform for displaying a specific user post
-    end
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
+    # Add any logic you want to perform for displaying a specific user post
   end
-  
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-    def show
-      @user = User.find(params[:id])
-      # Add any logic you want to perform for the user profile page
-    end
-  
-    def posts
-      @user = User.find(params[:user_id])
-      @posts = @user.posts
-      # Add any logic you want to perform for displaying user posts
-    end
+  def show
+    @user = User.find(params[:id])
+    # Add any logic you want to perform for the user profile page
   end
-  
+
+  def posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    # Add any logic you want to perform for displaying user posts
+  end
+end
