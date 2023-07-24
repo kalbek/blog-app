0b0fa808e7fe3e6eeea162e37443318ac752f058
kalbek <access2kaleb@gmail.com>
2023-07-24 12:04:21 +0300

    Add index action to user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+    # Add any additional logic you want for displaying all users
+  end
+  
   def show
     @user = User.find(params[:id])
     # Add any logic you want to perform for the user profile page
