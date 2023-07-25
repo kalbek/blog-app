dc842ae359cf63f69a0254e46c7c9e9ac73cd198
kalbek <access2kaleb@gmail.com>
2023-07-25 14:11:24 +0300

    Fix linter errors.

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,11 +1,10 @@
 # spec/factories/posts.rb
 FactoryBot.define do
-    factory :post do
-      title { Faker::Lorem.sentence }
-      text { Faker::Lorem.paragraph }
-      comments_counter { 0 }
-      likes_counter { 0 }
-      association :author, factory: :user
-    end
+  factory :post do
+    title { Faker::Lorem.sentence }
+    text { Faker::Lorem.paragraph }
+    comments_counter { 0 }
+    likes_counter { 0 }
+    association :author, factory: :user
   end
-  
+end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,40 +1,40 @@
 # spec/requests/users_spec.rb
 require 'rails_helper'
 
-RSpec.describe "Users", type: :request do
-  describe "GET /users" do
-    it "returns http success" do
+RSpec.describe 'Users', type: :request do
+  describe 'GET /users' do
+    it 'returns http success' do
       get users_path
       expect(response).to have_http_status(200)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       get users_path
       expect(response).to render_template(:index)
     end
 
-    it "includes correct placeholder text in the response body" do
+    it 'includes correct placeholder text in the response body' do
       get users_path
-      expect(response.body).to include("Welcome to Blog App")
+      expect(response.body).to include('Welcome to Blog App')
     end
   end
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      @user = FactoryBot.create(:user) 
+      @user = FactoryBot.create(:user)
     end
 
-    it "returns http success" do
+    it 'returns http success' do
       get user_path(@user)
       expect(response).to have_http_status(200)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       get user_path(@user)
       expect(response).to render_template(:show)
     end
 
-    it "includes correct user ID in the response body" do
+    it 'includes correct user ID in the response body' do
       get user_path(@user)
       expect(response.body).to include("User ID: #{@user.id}")
     end
