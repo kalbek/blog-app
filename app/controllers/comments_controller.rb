4c0d54fa5d8292c6cb3808a57c525af6d0c4684a
kalbek <access2kaleb@gmail.com>
2023-07-29 10:04:05 +0300

    Implement Create a comment on behalf of the current_user.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,24 +3,30 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment = current_user.comments.build
     @current_user = current_user
-  end
+    puts "post_id in new action: #{@post_id}" # Add this line to check the post_id value
+end
 
-  def create
-    puts "postttttttttt: #{params[:post_id]}"
-    @post = Post.find(params[:post_id])
+def create
+    @user = current_user
+    @post = Post.find_by(id: params[:comment][:post_id])
+    if @post.nil?
+        # Handle the case when the post is not found
+        redirect_to posts_url, alert: 'Error adding comment: Post not found.'
+        return
+      end
     @comment = @post.comments.build(comment_params)
-    @comment.user = current_user
+    @comment.author = current_user
     if @comment.save
-      redirect_to @post, notice: 'Comment added successfully.'
+      redirect_to user_posts_path(user_id: @user), notice: 'Comment added successfully.'
     else
       # Handle validation errors or other errors
-      redirect_to @post, alert: 'Error adding comment.'
+      redirect_to user_posts_path(user_id: @user), alert: 'Error adding comment.'
     end
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:text, :post_id)
   end    
 end
