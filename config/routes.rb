0183df32156b5fe91d9ac082fafcc8428db64113
kalbek <access2kaleb@gmail.com>
2023-07-29 13:54:03 +0300

    Implement add likes for posts.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:new, :create]
   resources :comments, only: [:new, :create]
-  resources :likes, only: [:create]
+  resources :likes, only: [:new, :create]
 
   # Defines the root path route ("/")
   root "users#index"
