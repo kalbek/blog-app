841f0849e6c4d299962a8002756fa8173237a55d
kalbek <access2kaleb@gmail.com>
2023-08-01 20:12:25 +0300

    Write remaining tests for user show page.

--- test/integration/users_view_test.rb
+++ test/integration/users_view_test.rb
@@ -46,14 +46,21 @@ class UsersViewTest < ActionDispatch::IntegrationTest
     end
 
     # Test if one can see a button that lets me view all of a user's posts.
-    # assert_selector 'a.btn.see-all-posts', text: 'See all posts', href: user_posts_path(@user)
+    assert page.has_link?('See all posts', href: user_posts_path(@user))
 
     # Test if one I click a user's post, it redirects me to that post's show page.
     click_on "user-post-link-#{@user.id}"
     assert_current_path(user_post_path(@user, users(:one).posts.first))
+  end
+
+  test "visiting a user's show page and clicking to see all posts" do
+    visit user_path(@user)
+
+    # Test if one can see a button that lets me view all of a user's posts.
+    assert page.has_link?('See all posts', href: user_posts_path(@user))
 
     # Test if one I click to see all posts, it redirects me to the user's post's index page.
-    # click_on 'See all posts'
-    # assert_current_path(user_posts_path(@user))
+    click_on 'See all posts'
+    assert_current_path(user_posts_path(@user))
   end
 end
