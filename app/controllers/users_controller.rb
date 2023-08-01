05b36c2588034345c744ae790cfc7dd34e770aff
kalbek <access2kaleb@gmail.com>
2023-08-01 08:30:05 +0300

    Remove unnecessary comments from user_controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,17 +2,14 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    # Add any additional logic you want for displaying all users
   end
 
   def show
     @user = User.find(params[:id])
-    # Add any logic you want to perform for the user profile page
   end
 
   def posts
     @user = User.find(params[:user_id])
     @posts = @user.posts
-    # Add any logic you want to perform for displaying user posts
   end
 end
