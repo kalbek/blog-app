152155569ea0e9e29926d5c96c9832867065f750
kalbek <access2kaleb@gmail.com>
2023-08-02 10:24:25 +0300

    Set roots in routes path.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :likes, only: [:new, :create]
 
   # Defines the root path route ("/")
-  root "users#index"
+  root to: 'home#index'
 
   get 'users', to: 'users#index', as: :users
 
