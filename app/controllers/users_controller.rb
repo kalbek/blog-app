5ed9e2f2b03e2ae76fb72d61a96d1d4b816f9fda
kalbek <access2kaleb@gmail.com>
2023-08-02 20:31:08 +0300

    Add style for login page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/users_controller.rb
 class UsersController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
 
   def index
     @users = User.includes(:posts).all
