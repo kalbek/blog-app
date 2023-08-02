6c905d7ae5c7e87c8c28d9aecb795f857487188e
kalbek <access2kaleb@gmail.com>
2023-08-02 11:40:15 +0300

    Configure OmniAuth.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -23,6 +23,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
+  config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
