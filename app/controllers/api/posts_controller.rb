692aa2d22b9cc934844ab5e88b777a79391f3c35
kalbek <access2kaleb@gmail.com>
2023-08-04 16:46:08 +0300

    Fix linters.

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,52 +1,55 @@
 # app/controllers/posts_controller.rb
-    class Api::PostsController < ApplicationController
-      before_action :authenticate_user!
-  
-      def index
-        @user = User.find(params[:user_id])
-        @posts = @user.posts.includes(:author).all
-    
-        respond_to do |format|
-          format.json { render json: @posts, status: :ok }
+class Api::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:author).all
+
+    respond_to do |format|
+      format.json { render json: @posts, status: :ok }
+    end
+  end
+
+  def show
+    @user = User.find(params[:user_id])
+    @post = @user.posts.includes(:author).find(params[:id])
+  end
+
+  def new
+    @post = current_user.posts.build
+    @current_user = current_user
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_post_path(@post.author, @post), notice: 'Post created successfully.' }
+        format.json { render json: @post, status: :created, location: user_post_path(@post.author, @post) }
+      else
+        format.html do
+          flash.now[:alert] = @post.errors.full_messages.join(', ')
+          render :new
         end
-      end
-  
-      def show
-        @user = User.find(params[:user_id])
-        @post = @user.posts.includes(:author).find(params[:id])
-      end
-  
-      def new
-        @post = current_user.posts.build
-        @current_user = current_user
-      end
-  
-      def create
-        @post = current_user.posts.build(post_params)
-        @post.comments_counter = 0
-        @post.likes_counter = 0
-  
-        respond_to do |format|
-          if @post.save
-            format.html { redirect_to user_post_path(@post.author, @post), notice: 'Post created successfully.' }
-            format.json { render json: @post, status: :created, location: user_post_path(@post.author, @post) }
-          else
-            format.html { flash.now[:alert] = @post.errors.full_messages.join(', '); render :new }
-            format.json { render json: @post.errors, status: :unprocessable_entity }
-          end
-        end
-      end
-  
-      def destroy
-        @post = Post.find(params[:id])
-        authorize! :destroy, @post # This line checks if the user is authorized to delete the post
-        @post.destroy
-        redirect_to root_path, notice: 'Post was successfully deleted.'
-      end
-  
-      private
-  
-      def post_params
-        params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+        format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    authorize! :destroy, @post # This line checks if the user is authorized to delete the post
+    @post.destroy
+    redirect_to root_path, notice: 'Post was successfully deleted.'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :comments_counter, :likes_counter)
+  end
+end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,21 +1,21 @@
 # app/controllers/users_controller.rb
 class Api::UsersController < ApplicationController
-    before_action :authenticate_user!
-  
-    def index
-      @users = User.includes(:posts).all
-      render json: @users
-    end
-  
-    def show
-      @user = User.includes(:posts).find(params[:id])
-      @users = User.all
-      render json: @users
-    end
-    
-    def posts
-        @user = User.find(params[:user_id])
-        @posts = @user.posts
-        render json: @posts
-    end
+  before_action :authenticate_user!
+
+  def index
+    @users = User.includes(:posts).all
+    render json: @users
   end
+
+  def show
+    @user = User.includes(:posts).find(params[:id])
+    @users = User.all
+    render json: @users
+  end
+
+  def posts
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+    render json: @posts
+  end
+end
